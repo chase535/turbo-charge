23f8ee35ba12d2abd950511d1652c3cc137e695d
诺鸡鸭 <1932263796@qq.com>
2023-01-03 15:43:11 +0800

    Revert "删除无用头文件"
    This reverts commit 2b4a3f2d765db1a67c664cde52dc8fc61c5b1114.

--- turbo-charge.c
+++ turbo-charge.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 void strrpc(char *str, char *oldstr, char *newstr)
 {
