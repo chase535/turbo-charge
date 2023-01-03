971e8571e8cf517f74d0c3d76d7057325db40859
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-01-03 14:38:39 +0800

    Update turbo-charge.c

--- turbo-charge.c
+++ turbo-charge.c
@@ -106,13 +106,13 @@ void charge_value(char *i)
 int main()
 {
     FILE *fp,*fq,*fm,*fa,*fb,*fc,*fd,*fe;
-    char **ppp,done[100],asdf[310],charge[100],uevent[3010],power[100],current_max[100],highest_temp_current[100],*buffer,constants[3010],msg[110],thermal[310],temps[3010],option[1010];
+    char **ppp,done[100],asdf[310],charge[100],uevent[3010],power[100],current_max[100],highest_temp_current[100],buffer[100],constants[3010],msg[110],thermal[310],temps[3010],option[1010];
     int file_num,i,done_int,asdf_int,wasd=0,charge_start,charge_stop,temp_ctrl,power_ctrl,power_int,recharge_temp,current_max_int,temp_max,highest_temp_current_int,temp_int,qwer;
     listDir("/sys/class/thermal", &ppp, &file_num);
     for(i=0;i<file_num;i++)
     {
         printf("%s\n",ppp[i]);
-        buffer=strcpy(ppp[i],"/type");
+        sprintf(buffer, "%s/type", ppp[i]);
         printf("%s\n",buffer);
         if(access(buffer, W_OK) != 0) continue;
         fq = fopen(buffer, "rt");
