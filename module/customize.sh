f3f9b297a94e8a4cc6952f31b37e5a23b03bc749
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-01-17 21:57:56 +0800

    解决跳电及断充问题(酷安@零漓殇度)

--- module/customize.sh
+++ module/customize.sh
@@ -36,91 +36,90 @@ ui_print "
 
 volume_keytest()
 {
-ui_print "--- 音量键测试 ---"
-ui_print "  请按音量+或-键"
-(/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > "$TMPDIR"/events) || return 1
-return 0
+    ui_print "--- 音量键测试 ---"
+    ui_print "  请按音量+或-键"
+    (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > "$TMPDIR"/events) || return 1
+    return 0
 }
 
 volume_choose()
 {
-while (true); do
-	/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > "$TMPDIR"/events
-	if (`cat "$TMPDIR"/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
-			break
-	fi
-done
-if (`cat "$TMPDIR"/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
-		return 1
-else
-		return 0
-fi
+    while true; do
+        /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > "$TMPDIR"/events
+        if (`cat "$TMPDIR"/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
+            break
+        fi
+    done
+    if (`cat "$TMPDIR"/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
+        return 1
+    else
+        return 0
+    fi
 }
 
 run_volume_key_test()
 {
-if volume_keytest; then
-	KEYTEST=volume_choose
-	ui_print "- 音量键测试完成"
-else
-	KEYTEST=false
-	ui_print " ！错误：没有检测到音量键选择，默认添加温控、不添加电量控制"
-fi
+    if volume_keytest; then
+        KEYTEST=volume_choose
+        ui_print "- 音量键测试完成"
+    else
+        KEYTEST=false
+        ui_print " ！错误：没有检测到音量键选择，默认添加温控、不添加电量控制"
+    fi
 }
 
 run_temp()
 {
-ui_print " "
-ui_print "--- 请选择是否添加温控（默认添加） ---"
-ui_print "  音量+键 = 添加温控，当温度高于52℃时限制最高充电电流为2A，低于45℃时恢复"
-ui_print "  音量-键 = 不添加温控，真·极速快充，高温伤手又伤机，谨慎选择！"
-ui_print " "
-if "$KEYTEST"; then
-	ui_print "- 不添加温控"
-	sed -i 's/TEMP_CTRL=1/TEMP_CTRL=0/g' $TMPDIR/option.txt
-else
-	ui_print "- 添加温控"
-fi
+    ui_print " "
+    ui_print "--- 请选择是否添加温控（默认添加） ---"
+    ui_print "  音量+键 = 添加温控，当温度高于52℃时限制最高充电电流为2A，低于45℃时恢复"
+    ui_print "  音量-键 = 不添加温控，真·极速快充，高温伤手又伤机，谨慎选择！"
+    ui_print " "
+    if "$KEYTEST"; then
+        ui_print "- 不添加温控"
+        sed -i 's/TEMP_CTRL=1/TEMP_CTRL=0/g' $TMPDIR/option.txt
+    else
+        ui_print "- 添加温控"
+    fi
 }
 
 run_power_ctrl()
 {
-ui_print " "
-ui_print "--- 请选择是否添加电量控制（默认不添加） ---"
-ui_print "  音量+键 = 不添加电量控制"
-ui_print "  音量-键 = 添加电量控制，默认为电量到达95%时断电，小于等于80%时恢复充电"
-ui_print " "
-if "$KEYTEST"; then
-	ui_print "- 添加电量控制"
-	sed -i 's/POWER_CTRL=0/POWER_CTRL=1/g' $TMPDIR/option.txt
-else
-	ui_print "- 不添加电量控制"
-fi
+    ui_print " "
+    ui_print "--- 请选择是否添加电量控制（默认不添加） ---"
+    ui_print "  音量+键 = 不添加电量控制"
+    ui_print "  音量-键 = 添加电量控制，默认为电量到达95%时断电，小于等于80%时恢复充电"
+    ui_print " "
+    if "$KEYTEST"; then
+        ui_print "- 添加电量控制"
+        sed -i 's/POWER_CTRL=0/POWER_CTRL=1/g' $TMPDIR/option.txt
+    else
+        ui_print "- 不添加电量控制"
+    fi
 }
 
 on_install()
 {
-[[ ! -d /data/adb/turbo-charge ]] && mkdir -p /data/adb/turbo-charge
-cp -af $TMPDIR/option.txt /data/adb/turbo-charge/option.txt
-[[ ! -d $MODPATH/system/bin ]] && mkdir -p $MODPATH/system/bin
-cp -af $TMPDIR/turbo-charge $MODPATH/system/bin/turbo-charge
-thermals=`ls /system/bin/*thermal* /system/etc/init/*thermal* /system/etc/perf/*thermal* /system/vendor/bin/*thermal* /system/vendor/etc/*thermal* /system/vendor/etc/powerhint* /system/vendor/etc/init/*_thermal* /system/vendor/etc/perf/*thermal* /system/vendor/lib/hw/thermal* /system/vendor/lib64/hw/thermal*`
-for thermal in $thermals
-do
-	[[ ! -d $MODPATH/${thermal%/*} ]] && mkdir -p $MODPATH/${thermal%/*}
-	touch $MODPATH/$thermal
-done
-[[ -f /data/current ]] && rm -rf /data/current
-chattr -i /data/vendor/thermal
-chattr -i /data/vendor/thermal/config
-rm -rf /data/vendor/thermal/config/*
-chattr +i /data/vendor/thermal/config
-chattr +i /data/vendor/thermal
+    [[ ! -d /data/adb/turbo-charge ]] && mkdir -p /data/adb/turbo-charge
+    cp -af $TMPDIR/option.txt /data/adb/turbo-charge/option.txt
+    [[ ! -d $MODPATH/system/bin ]] && mkdir -p $MODPATH/system/bin
+    cp -af $TMPDIR/turbo-charge $MODPATH/system/bin/turbo-charge
+    thermals=`ls /system/bin/*thermal* /system/etc/init/*thermal* /system/etc/perf/*thermal* /system/vendor/bin/*thermal* /system/vendor/etc/*thermal* /system/vendor/etc/powerhint* /system/vendor/etc/init/*_thermal* /system/vendor/etc/perf/*thermal* /system/vendor/lib/hw/thermal* /system/vendor/lib64/hw/thermal* 2>/dev/null | grep -v mi_thermald`
+    for thermal in $thermals; do
+        [[ ! -d $MODPATH/${thermal%/*} ]] && mkdir -p $MODPATH/${thermal%/*}
+        touch $MODPATH/$thermal
+    done
+    [[ -f /data/current ]] && rm -rf /data/current
+    chattr -i /data/vendor/thermal
+    chattr -i /data/vendor/thermal/config
+    rm -rf /data/vendor/thermal/config/*
+    chattr +i /data/vendor/thermal/config
+    chattr +i /data/vendor/thermal
 }
 
 set_permissions()
 {
-set_perm_recursive  $MODPATH  0  0  0755  0644
-chmod 0777 $MODPATH/system/bin/turbo-charge
-chmod 0777 /data/adb/turbo-charge/option.txt
+    set_perm_recursive  $MODPATH  0  0  0755  0644
+    chmod 0777 $MODPATH/system/bin/turbo-charge
+    chmod 0777 /data/adb/turbo-charge/option.txt
 }
