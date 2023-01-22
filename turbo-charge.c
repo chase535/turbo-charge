2c631353f4a47d5fc460c844ebb3bb9a69076ff4
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-01-23 00:43:20 +0800

    头文件改用引号

--- turbo-charge.c
+++ turbo-charge.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
-#include <unistd.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "dirent.h"
+#include "unistd.h"
 
 void strrpc(char *str, char *oldstr, char *newstr)
 {
