d1cf688387efafcc47babba441ac696d3091bb63
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-01-29 00:39:59 +0800

    更新执行代码

--- module/common/service.sh
+++ module/common/service.sh
@@ -1,4 +1,3 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
-echo "$(date "+%Y.%m.%d %H:%M:%S")" > /data/adb/turbo-charge/log.txt
-nohup $MODDIR/turbo-charge 2>&1 >> /data/adb/turbo-charge/log.txt &
+nohup $MODDIR/turbo-charge 2>&1 > /data/adb/turbo-charge/log.txt &
