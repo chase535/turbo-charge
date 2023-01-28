b657a26ee86be87018041018b70b27ca71fa140e
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-01-28 21:00:13 +0800

    将时间改为UTC+8，添加一处判断

--- turbo-charge.c
+++ turbo-charge.c
@@ -7,13 +7,78 @@
 #include "sys/types.h"
 #include "sys/stat.h"
 
-void printf_plus_time(char *dat)
+struct tm *get_time(int timezone)
 {
+    unsigned char tmp=0;
     time_t cur_time;
     struct tm *ptm;
     time(&cur_time);
-    ptm=localtime(&cur_time);
-    printf ("[ %04d.%02d.%02d %02d:%02d:%02d ] %s\n", (ptm->tm_year)+1900, (ptm->tm_mon)+1, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec, dat);
+    ptm=gmtime(&cur_time);
+    ptm->tm_year+=1900;
+    ptm->tm_mon+=1;
+    ptm->tm_hour+=8;
+    if(ptm->tm_hour > 23)
+    {
+        switch(ptm->tm_mon)
+        {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                tmp=1;
+                break;
+            case 2:
+                if(((ptm->tm_year%4 == 0) && (ptm->tm_year%100 != 0)) || (ptm->tm_year%400 == 0)) tmp=3;
+                else tmp=2;
+                break;
+            default:
+                tmp=0;
+                break;
+        }
+        ptm->tm_hour-=24;
+        ptm->tm_mday+=1;
+        if(tmp == 0)
+        {
+            if(ptm->tm_mday > 30)
+            {
+                ptm->tm_mday-=30;
+                ptm->tm_mon+=1;
+            }
+        }
+        else if(tmp == 1)
+        {
+            if(ptm->tm_mday > 31)
+            {
+                ptm->tm_mday-=31;
+                ptm->tm_mon+=1;
+            }
+        }
+        else if(tmp == 2)
+        {
+            if(ptm->tm_mday > 28)
+            {
+                ptm->tm_mday-=28;
+                ptm->tm_mon+=1;
+            }
+        }
+        else if(tmp == 3)
+        {
+            if(ptm->tm_mday > 29)
+            {
+                ptm->tm_mday-=29;
+                ptm->tm_mon+=1;
+            }
+        }
+        if(ptm->tm_mon > 12)
+        {
+            ptm->tm_mon-=12;
+            ptm->tm_year+=1;
+        }
+    }
+    return ptm;
+}
+
+void printf_plus_time(char *dat)
+{
+    struct tm *time_get=get_time(8);
+    printf("[ %04d.%02d.%02d %02d:%02d:%02d UTC+8 ] %s\n", time_get->tm_year, time_get->tm_mon, time_get->tm_mday, time_get->tm_hour, time_get->tm_min, time_get->tm_sec, dat);
 }
 
 void strrpc(char *str, char *oldstr, char *newstr)
@@ -470,7 +535,7 @@ int main()
         }
         else
         {
-            if(!tmp[2])
+            if(!tmp[2] && !tmp[1])
             {
                 printf_plus_time("充电器未连接");
                 tmp[1]=1;
