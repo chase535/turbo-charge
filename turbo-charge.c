6f71f353e26eff73847e4a488690fdc0e6bff99e
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-01-29 01:06:11 +0800

    printf后强制清空输出缓冲区，并把缓冲区内容输出。

--- turbo-charge.c
+++ turbo-charge.c
@@ -79,6 +79,7 @@ void printf_plus_time(char *dat)
 {
     struct tm *time_get=get_time(8);
     printf("[ %04d.%02d.%02d %02d:%02d:%02d UTC+8 ] %s\n", time_get->tm_year, time_get->tm_mon, time_get->tm_mday, time_get->tm_hour, time_get->tm_min, time_get->tm_sec, dat);
+    fflush(stdout);
 }
 
 void strrpc(char *str, char *oldstr, char *newstr)
