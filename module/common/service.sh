80f714f61527fec98999f89b2e9d013f8058a8e5
‘chase535’ <1932263796@qq.com>
2023-02-04 22:41:05 +0800

    等待手机启动完毕再执行程序，以确保时间准确

--- module/common/service.sh
+++ module/common/service.sh
@@ -1,3 +1,8 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
-nohup $MODDIR/turbo-charge 2>&1 > /data/adb/turbo-charge/log.txt &
+echo "等待手机启动完毕，以确保时间准确" > /data/adb/turbo-charge/log.txt
+until [[ "$(getprop service.bootanim.exit)" == "1" ]]; do
+	sleep 5
+done
+echo -e "手机启动完毕\n" >> /data/adb/turbo-charge/log.txt
+nohup $MODDIR/turbo-charge 2>&1 >> /data/adb/turbo-charge/log.txt &
