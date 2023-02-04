66652446859cdd3faa4bffd81192bd35d1209487
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-02-04 22:45:54 +0800

    更新描述

--- module/common/service.sh
+++ module/common/service.sh
@@ -1,8 +1,8 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
-echo "等待手机启动完毕，以确保时间准确" > /data/adb/turbo-charge/log.txt
+echo "等待手机启动完毕，以确保时间准确。若只看到这一行内容，请立即联系模块作者！" > /data/adb/turbo-charge/log.txt
 until [[ "$(getprop service.bootanim.exit)" == "1" ]]; do
-	sleep 5
+    sleep 5
 done
 echo -e "手机启动完毕\n" >> /data/adb/turbo-charge/log.txt
 nohup $MODDIR/turbo-charge 2>&1 >> /data/adb/turbo-charge/log.txt &
