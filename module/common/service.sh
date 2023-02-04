c447e3ddbdacd5e0f8154e84cec87b37e9a10266
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-02-04 23:19:48 +0800

    将检测开机完成的时间间隔改为1秒

--- module/common/service.sh
+++ module/common/service.sh
@@ -2,7 +2,7 @@
 MODDIR=${0%/*}
 echo "等待手机启动完毕，以确保时间准确。若只看到这一行内容，请立即联系模块作者！" > /data/adb/turbo-charge/log.txt
 until [[ "$(getprop service.bootanim.exit)" == "1" ]]; do
-    sleep 5
+    sleep 1
 done
 echo -e "手机启动完毕\n" >> /data/adb/turbo-charge/log.txt
 nohup $MODDIR/turbo-charge 2>&1 >> /data/adb/turbo-charge/log.txt &
