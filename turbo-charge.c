5f1b4c2770ad22432bbd32291d268047decbf08f
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-02-24 10:41:46 +0800

    添加一处判定

--- turbo-charge.c
+++ turbo-charge.c
@@ -136,7 +136,7 @@ void read_option(uint *last_modify_time, uchar num, uchar is_temp_wall)
 {
     FILE *fc;
     char option_tmp[42], option[100], *value;
-    uchar opt,value_stat[OPTION_QUANTITY]={0},i;
+    uchar opt,value_stat[OPTION_QUANTITY]={10},i;
     struct stat statbuf;
     check_read_file(option_file);
     stat(option_file, &statbuf);
@@ -151,6 +151,7 @@ void read_option(uint *last_modify_time, uchar num, uchar is_temp_wall)
         {
             snprintf(option_tmp, 42, "%s=", options[opt]);
             if(strstr(option, option_tmp) == NULL) continue;
+            value_stat[opt]=0;
             if(strcmp(option, option_tmp) == 0) value_stat[opt]=1;
             else
             {
@@ -178,6 +179,7 @@ void read_option(uint *last_modify_time, uchar num, uchar is_temp_wall)
             if(value_stat[opt] == 1) snprintf(chartmp, PRINTF_WITH_TIME_MAX_SIZE, "%s的值为空，故程序沿用上一次的值%d", options[opt], opt_new[opt]);
             else if(value_stat[opt] == 2) snprintf(chartmp, PRINTF_WITH_TIME_MAX_SIZE, "%s的值不是由纯数字组成，故程序沿用上一次的值%d", options[opt], opt_new[opt]);
             else if(value_stat[opt] == 3) snprintf(chartmp, PRINTF_WITH_TIME_MAX_SIZE, "%s的值小于0，这是不被允许的，故程序沿用上一次的值%d", options[opt], opt_new[opt]);
+            else if(value_stat[opt] == 4) snprintf(chartmp, PRINTF_WITH_TIME_MAX_SIZE, "%s不存在，故程序沿用上一次的值%d", options[opt], opt_new[opt]);
             if(value_stat[opt]) printf_with_time(chartmp);
             if(opt_old[opt] != opt_new[opt])
             {
@@ -193,9 +195,10 @@ void read_option(uint *last_modify_time, uchar num, uchar is_temp_wall)
     {
         for(opt=0;opt < OPTION_QUANTITY;opt++)
         {
-            if(value_stat[opt] == 1) snprintf(chartmp, PRINTF_WITH_TIME_MAX_SIZE, "配置文件中%s不存在或值为空，故程序使用默认值%d", options[opt], opt_new[opt]);
+            if(value_stat[opt] == 1) snprintf(chartmp, PRINTF_WITH_TIME_MAX_SIZE, "配置文件中%s的值为空，故程序使用默认值%d", options[opt], opt_new[opt]);
             else if(value_stat[opt] == 2) snprintf(chartmp, PRINTF_WITH_TIME_MAX_SIZE, "配置文件中%s的值不是由纯数字组成，故程序使用默认值%d", options[opt], opt_new[opt]);
             else if(value_stat[opt] == 3) snprintf(chartmp, PRINTF_WITH_TIME_MAX_SIZE, "配置文件中%s的值小于0，这是不被允许的，故程序使用默认值%d", options[opt], opt_new[opt]);
+            else if(value_stat[opt] == 4) snprintf(chartmp, PRINTF_WITH_TIME_MAX_SIZE, "配置文件中%s不存在，故程序使用默认值%d", options[opt], opt_new[opt]);
             if(value_stat[opt]) printf_with_time(chartmp);
             opt_old[opt]=opt_new[opt];
         }
