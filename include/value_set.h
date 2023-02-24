1de0d0eb9dd0e6d1e77688489d79b400edd3eb9b
‘chase535’ <1932263796@qq.com>
2023-02-24 12:40:12 +0800

    优化代码

--- include/value_set.h
+++ include/value_set.h
@@ -2,9 +2,9 @@
 #define _VALUE_SET_H
 
 #include "main.h"
-#include "sys/stat.h"
 
-#include "printf_with_time.h"
+#include "sys/stat.h"
+#include "unistd.h"
 
 void set_value(char *file, char *numb);
 void set_array_value(char **file, int num, char *value);
