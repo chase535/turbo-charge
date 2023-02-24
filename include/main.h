eaac81d7494d50b3f74afaa30beb7812b5bd8611
‘chase535’ <1932263796@qq.com>
2023-02-24 12:20:55 +0800

    test

--- include/main.h
+++ include/main.h
@@ -7,13 +7,11 @@
 typedef unsigned char uchar;
 typedef unsigned int uint;
 
-static uint opt_old[OPTION_QUANTITY]={0},opt_new[OPTION_QUANTITY]={0,1,0,50000000,15,95,80,52,2000000,45};
-static uchar tmp[5]={0};
-static char chartmp[PRINTF_WITH_TIME_MAX_SIZE];
-static char option_file[]="/data/adb/turbo-charge/option.txt";
-static char options[OPTION_QUANTITY][40]={"STEP_CHARGING_DISABLED","TEMP_CTRL","POWER_CTRL","CURRENT_MAX",
-                                        "STEP_CHARGING_DISABLED_THRESHOLD","CHARGE_STOP","CHARGE_START",
-                                        "TEMP_MAX","HIGHEST_TEMP_CURRENT","RECHARGE_TEMP"};
+extern uint opt_old[OPTION_QUANTITY],opt_new[OPTION_QUANTITY];
+extern uchar tmp[5];
+extern char chartmp[PRINTF_WITH_TIME_MAX_SIZE];
+extern char option_file[];
+extern char options[OPTION_QUANTITY][40];
 
 void free_celloc_memory(char ***addr, int num);
 void line_feed(char *line);
--- src/main.c
+++ src/main.c
@@ -14,6 +14,14 @@
 #include "some_ctrl.h"
 #include "printf_with_time.h"
 
+uint opt_old[OPTION_QUANTITY]={0},opt_new[OPTION_QUANTITY]={0,1,0,50000000,15,95,80,52,2000000,45};
+uchar tmp[5]={0};
+char chartmp[PRINTF_WITH_TIME_MAX_SIZE];
+char option_file[]="/data/adb/turbo-charge/option.txt";
+char options[OPTION_QUANTITY][40]={"STEP_CHARGING_DISABLED","TEMP_CTRL","POWER_CTRL","CURRENT_MAX",
+                                    "STEP_CHARGING_DISABLED_THRESHOLD","CHARGE_STOP","CHARGE_START",
+                                    "TEMP_MAX","HIGHEST_TEMP_CURRENT","RECHARGE_TEMP"};
+
 void free_celloc_memory(char ***addr, int num)
 {
     if(addr != NULL && *addr != NULL)
