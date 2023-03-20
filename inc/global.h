4ab5f98c502b5a32946b8747f0274711a2af3350
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-03-20 22:27:34 +0800

    修复错误

--- inc/global.h
+++ inc/global.h
@@ -1,8 +1,7 @@
 #define OPTION_QUANTITY 11
 #define PRINTF_WITH_TIME_MAX_SIZE 400
 
-extern unsigned int opt_old[OPTION_QUANTITY],opt_new[OPTION_QUANTITY];
-extern unsigned char tmp[5];
+extern int opt_old[OPTION_QUANTITY],opt_new[OPTION_QUANTITY];
 extern char chartmp[PRINTF_WITH_TIME_MAX_SIZE];
 extern char option_file[];
 extern char options[OPTION_QUANTITY][40];
