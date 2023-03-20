c77816003e97b53788d23b45b1d872bd3097e449
‘chase535’ <1932263796@qq.com>
2023-03-20 21:51:38 +0800

    修复错误

--- inc/global.h
+++ inc/global.h
@@ -6,3 +6,4 @@ extern unsigned char tmp[5];
 extern char chartmp[PRINTF_WITH_TIME_MAX_SIZE];
 extern char option_file[];
 extern char options[OPTION_QUANTITY][40];
+extern char temp_sensors[12][15];
