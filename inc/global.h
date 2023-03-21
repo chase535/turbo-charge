ec7369fe1e0279966b37dc60e0185411052dd8d3
chase535 <1932263796@qq.com>
2023-03-21 12:53:05 +0800

    修复错误

--- inc/global.h
+++ inc/global.h
@@ -1,6 +1,12 @@
 #define OPTION_QUANTITY 11
 #define PRINTF_WITH_TIME_MAX_SIZE 400
 
+struct option_struct
+{
+    char name[];
+    int value;
+};
+
 extern struct option options[OPTION_QUANTITY];
 extern char chartmp[PRINTF_WITH_TIME_MAX_SIZE];
 extern char option_file[];
--- src/global.c
+++ src/global.c
@@ -1,22 +1,17 @@
 #include "global.h"
 
-struct option
-{
-    char name[40];
-    int value;
-};
+struct option_struct options[OPTION_QUANTITY]={{"CYCLE_TIME",5},
+                                                {"STEP_CHARGING_DISABLED",0},
+                                                {"TEMP_CTRL",1},
+                                                {"POWER_CTRL",0},
+                                                {"CURRENT_MAX",50000000},
+                                                {"STEP_CHARGING_DISABLED_THRESHOLD",15},
+                                                {"CHARGE_STOP",95},
+                                                {"CHARGE_START",80},
+                                                {"TEMP_MAX",52},
+                                                {"HIGHEST_TEMP_CURRENT",2000000},
+                                                {"RECHARGE_TEMP",45}};
 
-struct option options[OPTION_QUANTITY]={{"CYCLE_TIME",5},
-                                        {"STEP_CHARGING_DISABLED",0},
-                                        {"TEMP_CTRL",1},
-                                        {"POWER_CTRL",0},
-                                        {"CURRENT_MAX",50000000},
-                                        {"STEP_CHARGING_DISABLED_THRESHOLD",15},
-                                        {"CHARGE_STOP",95},
-                                        {"CHARGE_START",80},
-                                        {"TEMP_MAX",52},
-                                        {"HIGHEST_TEMP_CURRENT",2000000},
-                                        {"RECHARGE_TEMP",45}};
 char chartmp[PRINTF_WITH_TIME_MAX_SIZE];
 char option_file[]="/data/adb/turbo-charge/option.txt";
 char temp_sensors[12][15]={"lcd_therm","conn_therm","modem_therm","wifi_therm","quiet_therm","mtktsbtsnrpa",
