70a8cf0ddc43fd1df1b8517455b077469cc2ae9a
chase535 <1932263796@qq.com>
2023-09-24 17:01:19 +0800

    Revert "修复编译错误"
    This reverts commit 8b8084e2f8658c124224e9c7b92bc0d561d42f6b.

--- inc/my_thread.h
+++ inc/my_thread.h
@@ -1,8 +1,6 @@
 #ifndef _MY_THREAD_H
 #define _MY_THREAD_H
 
-#include "my_thread.h"
-
 extern pthread_mutex_t mutex_foreground_app,mutex_options;
 
 #endif
--- src/my_thread.c
+++ src/my_thread.c
@@ -1,4 +1,6 @@
 #include "pthread.h"
 
+#include "my_thread.h"
+
 //创建两个互斥锁，供多线程使用
 pthread_mutex_t mutex_foreground_app,mutex_options;
