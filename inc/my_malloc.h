07aec4042a0edb5a366921b50b9d2ca598d39669
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2024-01-11 16:59:19 +0800

    修复编译错误

--- inc/my_malloc.h
+++ inc/my_malloc.h
@@ -1,8 +1,8 @@
 #ifndef _MY_MALLOC_H
 #define _MY_MALLOC_H
 
-void *(*my_calloc)(size_t, size_t);
-void *(*my_realloc)(void *, size_t);
-void (*my_free)(void *);
+extern void *(*my_calloc)(size_t, size_t);
+extern void *(*my_realloc)(void *, size_t);
+extern void (*my_free)(void *);
 
 #endif
