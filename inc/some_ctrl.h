c30f5c371bf1f2f6721044d03670c610f154d384
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2024-02-16 20:00:08 +0800

    统一形参类型

--- inc/some_ctrl.h
+++ inc/some_ctrl.h
@@ -6,6 +6,6 @@
 void step_charge_ctl(char *value);
 void charge_ctl(char *i);
 void powel_ctl(int *last_charge_stop, int *charge_is_stop);
-void bypass_charge_ctl(pthread_t *thread1, int *android_version, char last_appname[100], int *is_bypass, int *screen_is_off, char **current_max_file, int current_max_file_num);
+void bypass_charge_ctl(pthread_t *thread1, int *android_version, char *last_appname, int *is_bypass, int *screen_is_off, char **current_max_file, int current_max_file_num);
 
 #endif
