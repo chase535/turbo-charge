ab0baf2bd5056be2a35e45f1a3d58583bfabef37
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2024-02-18 13:57:57 +0800

    修复编译错误

--- src/my_malloc.c
+++ src/my_malloc.c
@@ -1,16 +1,16 @@
 #ifdef MI_MALLOC
-#include "mimalloc.h"
+#include <mimalloc.h>
 void *(*my_calloc)(size_t, size_t)=&mi_calloc;
 void *(*my_realloc)(void *, size_t)=&mi_realloc;
 void (*my_free)(void *)=&mi_free;
 #else
-#include "malloc.h"
+#include <malloc.h>
 void *(*my_calloc)(size_t, size_t)=&calloc;
 void *(*my_realloc)(void *, size_t)=&realloc;
 void (*my_free)(void *)=&free;
 #endif
 
-#include "stdlib.h"
+#include <stdlib.h>
 
 //完全释放动态申请的二级指针的内存
 void free_malloc_memory(char ***addr, int num)
