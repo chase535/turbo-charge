e5c1eda9e86dbdab2dd066b558e324ce5ed1c236
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2026-03-08 23:38:25 +0800

    将判断各个所需文件封装为单独的函数，且不再使用POSIX的正则表达式库

--- inc/main.h
+++ inc/main.h
@@ -12,5 +12,11 @@ int list_dir(char *path, char ***ppp);
 void line_feed(char *line);
 void check_read_file(char *file);
 void read_file(char *file_path, char *char_var, int max_char_num);
+void check_required_files(uchar *battery_status, uchar *battery_capacity, uchar *power_control,
+                          uchar *step_charge, uchar *force_temp, uchar *current_change,
+                          int *temp_sensor_num, char *temp_sensor,
+                          char ***current_max_file, int *current_max_file_num,
+                          char ***current_limit_file, int *current_limit_file_num,
+                          char ***temp_file, int *temp_file_num);
 
 #endif
