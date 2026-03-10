b6a97a46c39f1880ede22b5b37f6406962a9c79c
chase535 <1932263796@qq.com>
2026-03-10 22:06:09 +0800

    使用mmap和inotify事件优化配置文件读取

--- src/read_option_file.c
+++ src/read_option_file.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/inotify.h>
+#include <sys/select.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <limits.h>
 
 #include "read_option_file.h"
 #include "printf_with_time.h"
@@ -13,12 +18,25 @@
 */
 void *read_option_file(void *arg)
 {
-    //文件修改时间需持续存储，所以在循环体外定义
-    uint option_last_modify_time=0, sleep_time=*(int *)arg;
+    int sleep_time=*(int *)arg, first_run=1;
+    int ifd=inotify_init1(IN_CLOEXEC | IN_NONBLOCK);
+    inotify_add_watch(ifd, option_file, IN_CLOSE_WRITE | IN_MODIFY);
     while(1)
     {
+        if(!first_run)
+        {
+            //使用 select 阻塞等待 inotify 事件，超时时间为 sleep_time 秒
+            //文件未修改时线程持续休眠，不产生任何系统调用开销
+            fd_set rfds;
+            struct timeval tv={sleep_time, 0};
+            FD_ZERO(&rfds);
+            FD_SET(ifd, &rfds);
+            if(select(ifd+1, &rfds, NULL, NULL, &tv) <= 0) continue;
+            //排空 inotify 事件队列，防止积压
+            char evbuf[sizeof(struct inotify_event)+NAME_MAX+1];
+            while(read(ifd, evbuf, sizeof(evbuf)) > 0);
+        }
         //在循环体内定义变量，这样变量仅存在于单次循环，每次循环结束后变量自动释放，循环开始时变量重新定义
-        FILE *fc;
         char option_tmp[42]={0},option[100]={0},*value=NULL;
         uchar opt=0,i=0,option_quantity=1;
         int new_value=0;
@@ -33,59 +51,68 @@ void *read_option_file(void *arg)
         //不定长数组无法在定义时初始化为全0，所以后续会使用memset进行清零
         uchar value_stat[option_quantity];
         check_read_file(option_file);
-        //先获取文件修改时间，若本次的文件修改时间与上次相等，证明配置文件未修改，跳过读取
-        stat(option_file, &statbuf);
-        if(statbuf.st_mtime == option_last_modify_time)
-        {
-            sleep(sleep_time);
-            continue;
-        }
         memset(value_stat, 0, sizeof(value_stat));
         pthread_mutex_lock((pthread_mutex_t *)&mutex_options);
-        fc=fopen(option_file, "rt");
-        while(fgets(option, sizeof(option), fc) != NULL)
+        int fd=open(option_file, O_RDONLY);
+        fstat(fd, &statbuf);
+        if(statbuf.st_size > 0)
         {
-            line_feed(option);
-            //跳过以英文井号开头的行及空行
-            if(!strlen(option) || (strstr(option, "#") != NULL && !strstr(option, "#"))) continue;
-            for(opt=0,node=options_head.next;node;opt++,node=node->next)
+            char *map=mmap(NULL, statbuf.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+            close(fd);
+            char *pos=map, *end=map+statbuf.st_size, *line_end;
+            while(pos < end)
             {
-                //将配置名与等号进行拼接，用来进行匹配
-                snprintf(option_tmp, 42, "%s=", node->name);
-                if(strstr(option, option_tmp) == NULL) continue;
-                //value_stat数组存储配置文件中每个变量值的状态，详情直接看后面判断value_stat数组的值的相关代码
-                value_stat[opt]=10;
-                //判断变量值是否合法，合法则对程序内部变量进行赋值，否则将状态存入value_stat数组中
-                if(!strcmp(option, option_tmp)) value_stat[opt]=1;
-                else
+                line_end=memchr(pos, '\n', end-pos);
+                size_t line_len=line_end ? (size_t)(line_end-pos) : (size_t)(end-pos);
+                if(line_len >= sizeof(option)) line_len=sizeof(option)-1;
+                memcpy(option, pos, line_len);
+                option[line_len]='\0';
+                pos=line_end ? line_end+1 : end;
+                line_feed(option);
+                //跳过以英文井号开头的行及空行
+                if(!strlen(option) || (strstr(option, "#") != NULL && !strstr(option, "#"))) continue;
+                for(opt=0,node=options_head.next;node;opt++,node=node->next)
                 {
-                    value=option+strlen(option_tmp);
-                    for(i=0;i < strlen(value);i++)
+                    //将配置名与等号进行拼接，用来进行匹配
+                    snprintf(option_tmp, 42, "%s=", node->name);
+                    if(strstr(option, option_tmp) == NULL) continue;
+                    //value_stat数组存储配置文件中每个变量值的状态，详情直接看后面判断value_stat数组的值的相关代码
+                    value_stat[opt]=10;
+                    //判断变量值是否合法，合法则对程序内部变量进行赋值，否则将状态存入value_stat数组中
+                    if(!strcmp(option, option_tmp)) value_stat[opt]=1;
+                    else
                     {
-                        if((value[i] < '0' || value[i] > '9') && ((!i && (value[i] != '-' || strlen(value) == 1)) || i))
+                        value=option+strlen(option_tmp);
+                        for(i=0;i < strlen(value);i++)
                         {
-                            value_stat[opt]=2;
-                            break;
+                            if((value[i] < '0' || value[i] > '9') && ((!i && (value[i] != '-' || strlen(value) == 1)) || i))
+                            {
+                                value_stat[opt]=2;
+                                break;
+                            }
                         }
                     }
-                }
-                if(value_stat[opt] != 10) continue;
-                new_value=atoi(value);
-                if(new_value < 0) value_stat[opt]=3;
-                else if(!strcmp((char *)(node->name), "CYCLE_TIME") && !new_value) value_stat[opt]=4;
-                if(value_stat[opt] == 10 && node->value != new_value)
-                {
-                    node->value=new_value;
-                    value_stat[opt]=100;
+                    if(value_stat[opt] != 10) continue;
+                    new_value=atoi(value);
+                    if(new_value < 0) value_stat[opt]=3;
+                    else if(!strcmp((char *)(node->name), "CYCLE_TIME") && !new_value) value_stat[opt]=4;
+                    if(value_stat[opt] == 10 && node->value != new_value)
+                    {
+                        node->value=new_value;
+                        value_stat[opt]=100;
+                    }
                 }
             }
+            munmap(map, statbuf.st_size);
         }
-        fclose(fc);
-        fc=NULL;
+        else
+        {
+            close(fd);
+        }
         for(opt=0,node=options_head.next;node;opt++,node=node->next)
         {
             //通过判断是否第一次运行来进行不同的字符串输出
-            if(option_last_modify_time)
+            if(!first_run)
             {
                 if(value_stat[opt] == 0) printf_with_time("配置文件中%s不存在，故程序沿用上一次的值%d", node->name, node->value);
                 else if(value_stat[opt] == 1) printf_with_time("新%s的值为空，故程序沿用上一次的值%d", node->name, node->value);
@@ -103,9 +130,8 @@ void *read_option_file(void *arg)
                 else if(value_stat[opt] == 4) printf_with_time("配置文件中%s的值为0，这是不被允许的，故程序使用默认值%d", node->name, node->value);
             }
         }
-        option_last_modify_time=statbuf.st_mtime;
+        first_run=0;
         pthread_mutex_unlock((pthread_mutex_t *)&mutex_options);
-        sleep(sleep_time);
     }
     return NULL;
 }
